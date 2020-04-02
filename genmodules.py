96dccc6214f27ef93ffffdd42f4e479fa2e843f5
Kevin Mittman <kmittman@nvidia.com>
2020-04-01 20:30:27 -0700

    Warning if kmod packages missing
    Signed-off-by: Kevin Mittman <kmittman@nvidia.com>

--- genmodules.py
+++ genmodules.py
@@ -329,8 +329,12 @@ if __name__ == '__main__':
                 print('WARNING: RPM kmod-nvidia-latest-dkms in version ' + branch.version() + ' not found')
         else:
             # All the kmod rpms which belong to this branch
-            for rpm in filter(lambda r: kmod_belongs_to(r, branch), kmod_rpms):
-                out.tab().tab().tab().line('- ' + filename_to_nevra(rpm, repodir))
+            kmod_rpms = list(filter(lambda r: kmod_belongs_to(r, branch), kmod_rpms))
+            if not kmod_rpms:
+                print('WARNING: Branch %s in version %s is not a DKMS branch, but no precompiled kmod packages can be found' % (branch.name, branch.version()))
+            else:
+                for rpm in kmod_rpms:
+                    out.tab().tab().tab().line('- ' + filename_to_nevra(rpm, repodir))
 
         out.tab().line('profiles:')
         out.tab().tab().line('default:')
