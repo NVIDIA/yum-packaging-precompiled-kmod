b20498c19ba7cde5ef9ad093bf1c7f8582e3b2f6
kmittman <58795776+kmittman@users.noreply.github.com>
2021-09-07 20:23:18 -0700

    Merge pull request #26 from AndyDick/main
    Add fabric-manager component RPM and profile to aarch64 modularity st…

--- genmodules.py
+++ genmodules.py
@@ -399,6 +399,16 @@ if __name__ == '__main__':
                 out.tab().tab().tab().tab().line('- ' + 'nvidia-fabric-manager')
                 out.tab().tab().tab().tab().line('- ' + 'libnvidia-nscq-' + branch.major)
 
+        if branch.arch == "aarch64" and int(branch.major) > 470:
+            out.tab().tab().line('fm:')
+            out.tab().tab().tab().line('description: FabricManager installation')
+            out.tab().tab().tab().line('rpms:')
+            for pkg in sorted(existing_branch_pkgs):
+                out.tab().tab().tab().tab().line('- ' + pkg)
+            if branch.is_dkms():
+                out.tab().tab().tab().tab().line('- kmod-nvidia-latest-dkms')
+            out.tab().tab().tab().tab().line('- ' + 'nvidia-fabric-manager')
+
         out.tab().tab().line('ks:')
         out.tab().tab().tab().line('description: Installation via kickstart')
         out.tab().tab().tab().line('rpms:')
